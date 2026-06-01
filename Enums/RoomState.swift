f65a463d50986e47ae387110e53699bfca2a9249
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2026-06-01 11:35:21 +0700

    Remove inline comment from RoomState enum
    Remove a leftover Indonesian bugfix comment from the `waiting` case in Enums/RoomState.swift. This is a non-functional cleanup (no behavior changes) that tidies up the enum source.

--- Enums/RoomState.swift
+++ Enums/RoomState.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Represents the current life-cycle state of a sparring room.
 enum RoomState: String, Codable {
-    case waiting = "WAITING" // PERBAIKAN BUG: Menambahkan status menunggu jadwal
+    case waiting = "WAITING"
     case preparing = "PREPARING"
     case ongoing = "ONGOING"
     case done = "DONE"
